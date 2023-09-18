107b3e3042e02223d26c8faf95678e5f66309a92
Reynyuu2010 <uii21039@gmail.com>
2023-09-19 03:16:07 +0900

    トピックのsubscribeをcallback化

--- imu_transformer_node.cpp
+++ imu_transformer_node.cpp
@@ -1,7 +1,22 @@
 #include <ros/ros.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <tf2_ros/transform_listener.h>
 #include <sensor_msgs/Imu.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+ros::Publisher imu_pub; // パブリッシャーをグローバルスコープで宣言
+
+void callback(const sensor_msgs::Imu::ConstPtr& imu_msg)
+{
+    // カメラ座標系からcamera_link座標系への変換を取得
+    geometry_msgs::TransformStamped transform;
+    transform = tf_buffer.lookupTransform("camera_link", "camera_imu_optical_frame", ros::Time(0));
+
+    // IMUデータのorientation部分にトランスフォームを適用
+    tf2::doTransform(imu_msg.orientation, imu_msg.orientation, transform);
+
+    imu_pub.publish(imu_msg)
+}
 
 int main(int argc, char** argv)
 {
@@ -11,39 +26,9 @@ int main(int argc, char** argv)
     tf2_ros::Buffer tf_buffer;
     tf2_ros::TransformListener tf_listener(tf_buffer);
 
-    ros::Publisher imu_pub = nh.advertise<sensor_msgs::Imu>("/camera_link/imu", 10); // 新しいimuトピックをパブリッシュ
+    imu_pub = nh.advertise<sensor_msgs::Imu>("/camera_link/imu", 10); // パブリッシャーをグローバルスコープで初期化
+    ros::Subscriber imu_sub = nh.subscribe("/camera/imu", 10, callback);
 
-    ros::Rate rate(10.0); // ループの周期(Hz)
-
-    while (ros::ok())
-    {
-        sensor_msgs::Imu imu_data;
-        try
-        {
-            // IMUデータを購読
-            sensor_msgs::Imu imu_msg;
-            imu_msg = ros::topic::waitForMessage<sensor_msgs::Imu>("/camera/imu", nh);
-
-            // カメラ座標系からcamera_link座標系への変換を取得
-            geometry_msgs::TransformStamped transform;
-            transform = tf_buffer.lookupTransform("camera_link", "camera_imu_optical_frame", ros::Time(0));
-
-            // IMUデータのコピー
-            imu_data = imu_msg;
-
-            // IMUデータのorientation部分にトランスフォームを適用
-            tf2::doTransform(imu_data.orientation, imu_data.orientation, transform);
-
-            // 新しいimuデータをパブリッシュ
-            imu_pub.publish(imu_data);
-        }
-        catch (tf2::TransformException& ex)
-        {
-            ROS_WARN("%s", ex.what());
-        }
-
-        rate.sleep();
-    }
-
+    ros::spin();
     return 0;
 }
